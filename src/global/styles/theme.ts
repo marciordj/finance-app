067aa5257028fa998e06fdb01cff5a072f1979cf
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2021-06-03 00:19:00 -0300

    feature: add global theme

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,21 @@
+export default {
+  colors: {
+    primary: '#5636D3',
+
+    orange: '#FF872C',
+    orange_light: 'rgba(255, 135, 44, 0.3)',
+
+    succes: '#12A454',
+    success_light: 'rgba(18, 164, 84, 0.5)',
+
+    warning: '#E83F5B',
+    warning_light: 'rgba(232, 63, 91, 0.5)',
+
+    shape: '#FFFFFF',
+
+    title: '#363F5F',
+    text: '#969CB2',
+
+    background: '#F0F2F5'
+  }
+}
